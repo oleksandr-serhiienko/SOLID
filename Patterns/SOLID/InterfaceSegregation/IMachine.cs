f626ad5afe04a46502ab972978b657f2d3751b33
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 16:50:09 +0200

    I Interface sergregation

---
+++ Patterns/SOLID/InterfaceSegregation/IMachine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.SOLID.InterfaceSegregation
+{
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Load(Document d);
+        void Send(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IMultiFunctionalDevise : IScanner, IPrinter
+    {
+
+    }
+}
---
+++ Patterns/SOLID/InterfaceSegregation/MultiFumctionalPrinter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.SOLID.InterfaceSegregation
+{
+    public class MultiFumctionalPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Load(Document d)
+        {
+            //
+        }
+
+        public void Send(Document d)
+        {
+            //
+        }
+    }
+
+    public class MultiFunctionalMachine: IMultiFunctionalDevise
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionalMachine(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }
+    }
+
+    public  class OldFasionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+             
+        }
+
+        public void Load(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
