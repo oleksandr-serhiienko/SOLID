8ef0432f172ccc8e25ade961ac9e5ffcb8d935e8
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 16:48:34 +0200

    S Single repository principle

---
+++ Patterns/SOLID/SingleRepositoryPrinciple/JournalExaple.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Patterns.SingleRepositoryPrinciple
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        //public void Save(string fileName)
+        //{
+        //    File.WriteAllText(fileName, ToString());
+        //}
+
+        //public static Journal Load(string fileName)
+        //{
+        //    return new Journal();
+        //}
+
+        //public void Load(Uri uri)
+        //{ 
+        //}
+
+    }
+}
---
+++ Patterns/SOLID/SingleRepositoryPrinciple/Persistence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Patterns.SingleRepositoryPrinciple
+{
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool owerwrite = false)
+        {
+            if (owerwrite || !File.Exists(filename))
+            {
+                var ff = File.Exists(filename);
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
+}
