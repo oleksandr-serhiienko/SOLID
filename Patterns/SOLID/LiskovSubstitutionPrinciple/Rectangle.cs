d8bcf82a6dc177c16fdf2a471f6a6627c9fe5596
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-20 16:49:39 +0200

    L Liskov Principle

---
+++ Patterns/SOLID/LiskovSubstitutionPrinciple/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.SOLID.LiskovSubstitutionPrinciple
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+
+        public virtual int Height { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public Rectangle()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)} : {Height}";
+        }
+    }
+}
---
+++ Patterns/SOLID/LiskovSubstitutionPrinciple/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.SOLID.LiskovSubstitutionPrinciple
+{
+    public class Square : Rectangle
+    {
+        public Square(int width, int height) : base(width, height)
+        {
+        }
+
+
+        public Square()
+        {
+
+        }
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Height = base.Width = value; }
+        }
+    }
+}
